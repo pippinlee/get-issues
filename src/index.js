2d91393f133996b4c674b8d400f5632aed4e2889
Michael Perrotte <mike@mikecorp.ca>
2015-11-28 03:40:59 -0500

    fixed comments and added semicolons

--- src/index.js
+++ src/index.js
@@ -1,200 +1,216 @@
 #!/usr/bin/env node
 
-'use strict'
+'use strict';
 
-var async = require('async')
-var Repo = require('git-tools')
-var request = require('request')
-var fs = require('fs-extra')
-var figlet = require('figlet')
-var slug = require('slug')
-var util = require('util')
-var url = require('url')
-var sshURL = require('ssh-url')
-var options = require('./options')
-var inquirer = require('inquirer')
-var github = new (require('github'))({version: '3.0.0'})
-var os = require('os')
-var path = require('path')
+var async = require('async');
+var Repo = require('git-tools');
+var request = require('request');
+var fs = require('fs-extra');
+var figlet = require('figlet');
+var slugFactory = require('urlify');
+var util = require('util');
+var url = require('url');
+var sshURL = require('ssh-url');
+var options = require('./options');
+var inquirer = require('inquirer');
+var github = new (require('github'))({version: '3.0.0'});
+var os = require('os');
+var path = require('path');
 
-// attempt at a platform agnostic place to store auth tokens
-var configDir = path.resolve(os.homedir(), '.config' , 'get-issues')
-var configFile = path.resolve(configDir, 'setup.json')
+// INFO: attempt at a platform agnostic place to store auth tokens
+var configDir = path.resolve(os.homedir(), '.config' , 'get-issues');
+var configFile = path.resolve(configDir, 'setup.json');
 
-require('./addToGitignore.js')
-require('colors')
+require('./addToGitignore.js');
+require('colors');
 
-// set options for github api requests
+// INFO: set options for slug config
+var slugOptions = {
+  addEToUmlauts: true,
+  szToSs: true,
+  spaces: '-',
+  toLower: true,
+  nonPrintable: '.',
+  trim: true
+  // INFO: dev options
+  // failureOutput: 'nothing',
+  // extendString: true
+};
+
+// INFO: instanciate the slug factory
+var slug = slugFactory.create(slugOptions);
+
+// INFO: set options for github api requests
 var options = {
   headers: {
     'User-Agent': 'request'
   }
-}
+};
 
-// set options for github api requests
+// INFO: set options for github api requests
 var callURL = function (url, cb) {
   var options = {
     url: url,
     headers: {
       'User-Agent': 'request'
     }
-  }
+  };
 
   function callback (error, response, body) {
     if (!error && response.statusCode === 200) {
-      var info = JSON.parse(body)
-      cb(null, info)
+      var info = JSON.parse(body);
+      cb(null, info);
     }
   }
-  request(options, callback)
-}
+  request(options, callback);
+};
 
 
 async.waterfall([
 
-  // check if /issues folder needs to be created
+  // INFO: check if /issues folder needs to be created
   function init (cb) {
     if (!fs.existsSync('./issues')) {
-      fs.mkdirSync('./issues')
+      fs.mkdirSync('./issues');
     }
 
-    cb(null, 'gh-folder already exists')
+    cb(null, 'gh-folder already exists');
   },
 
-  // get remote github url for use with api
+  // INFO: get remote github url for use with api
   function getGithubURL (init, cb) {
-    var repo = new Repo(__dirname)
-    repo.remotes(function ( error, remotes) {
+    var repo = new Repo(__dirname);
+    repo.remotes(function (error, remotes) {
       if (error) {
-        cb(error, null)
+        cb(error, null);
       }
-      var remoteURL = remotes[0].url
-      cb(null, remoteURL)
-    })
+      var remoteURL = remotes[0].url;
+      cb(null, remoteURL);
+    });
   },
 
   function makeApiUrl (remoteURL, cb) {
-    var parsedURL = url.parse(remoteURL)
-    var currentRepoInfo = {}
+    var parsedURL = url.parse(remoteURL);
+    var currentRepoInfo = {};
 
-    // handle ssh remote URL
+    // INFO: handle ssh remote URL
     if (!parsedURL.protocol) {
-      parsedURL = sshURL.parse(remoteURL)
+      parsedURL = sshURL.parse(remoteURL);
       var finalURL = url.format({
         protocol: 'https',
         host: util.format('api.%s', parsedURL.hostname),
         pathname: `repos${parsedURL.pathname.split('.')[0]}/issues`
-      })
-      currentRepoInfo.username = parsedURL.pathname.split('/')[1]
-      currentRepoInfo.repo = path.basename(remoteURL, '.git')
+      });
+      currentRepoInfo.username = parsedURL.pathname.split('/')[1];
+      currentRepoInfo.repo = path.basename(remoteURL, '.git');
     } else {
-      // handle https remote URL
-      var splitURL = parsedURL.pathname.split('/')
+      // INFO: handle https remote URL
+      var splitURL = parsedURL.pathname.split('/');
       var finalURL = url.format({
         protocol: parsedURL.protocol,
         host: util.format('api.%s', parsedURL.host),
         pathname: `/repos/${splitURL[1]}/${splitURL[2].split('.')[0]}/issues`
-      })
-      currentRepoInfo.username = parsedURL.pathname.split('/')[1]
-      currentRepoInfo.repo = path.basename(remoteURL, '.git')
+      });
+      currentRepoInfo.username = parsedURL.pathname.split('/')[1];
+      currentRepoInfo.repo = path.basename(remoteURL, '.git');
     }
 
-    cb(null, finalURL, currentRepoInfo)
+    cb(null, finalURL, currentRepoInfo);
   },
 
-  // make request to remote repo's issue page
+  // INFO: make request to remote repo's issue page
   function getIssues (finalURL, currentRepoInfo, cb) {
 
-    options.url = finalURL
+    options.url = finalURL;
     function callback (error, response, body) {
       if (error) {
-        cb(error, null)
+        cb(error, null);
       }
       if (!error && response.statusCode !== 200 ) {
-        cb(error, 'invalid repo', currentRepoInfo)
+        cb(error, 'invalid repo', currentRepoInfo);
       }
       if (!error && response.statusCode === 200) {
-        var info = JSON.parse(body)
-        var filterOutPR = []
+        var info = JSON.parse(body);
+        var filterOutPR = [];
         info.forEach(function (issue) {
           if (!issue.pull_request) {
-            filterOutPR.push(issue)
+            filterOutPR.push(issue);
           }
-        })
-        cb(null, filterOutPR, currentRepoInfo)
+        });
+        cb(null, filterOutPR, currentRepoInfo);
       }
     }
 
-    request(options, callback)
+    request(options, callback);
   },
 
-  // if repo is private, checks for github auth token
+  // INFO: if repo is private, checks for github auth token
   function checkIfPrivateRepo (filterOutPR, currentRepoInfo, cb) {
 
     if (filterOutPR === 'invalid repo') {
-      // let's check if this token already exits
+      // INFO: let's check if this token already exits
       fs.readJSON(configFile, function(err, obj) {
           if (err) {
-            var tokenCheck = true
-            // no token found
-            cb(null, filterOutPR, tokenCheck, currentRepoInfo)
+            var tokenCheck = true;
+            // INFO: no token found
+            cb(null, filterOutPR, tokenCheck, currentRepoInfo);
           } else {
-            var tokenCheck = obj.token
-            // found token that exits already
-            cb(null, filterOutPR, tokenCheck, currentRepoInfo)
+            var tokenCheck = obj.token;
+            // INFO: found token that exits already
+            cb(null, filterOutPR, tokenCheck, currentRepoInfo);
           }
-        })
+        });
     } else if (filterOutPR) {
-      var tokenCheck = false
-      cb(null, filterOutPR, tokenCheck, currentRepoInfo)
+      var tokenCheck = false;
+      cb(null, filterOutPR, tokenCheck, currentRepoInfo);
     }
   },
 
-  // if token is need and doesn't exists create it
+  // INFO: if token is need and doesn't exists create it
   function getTokenIfNeeded(filterOutPR, tokenCheck, currentRepoInfo, cb) {
-    var tokenFinal
+    var tokenFinal;
 
-    // if tokenCheck is true, need to get token prompts user
+    // INFO: if tokenCheck is true, need to get token prompts user
     if (tokenCheck === true){
-      // it's a private repo
+      // INFO: it's a private repo
       var questions = [
         {
           type: "input",
           name: "token",
           message: "https://github.com/settings/tokens/new?scopes=repo&description=get%20issues%20CLI".red.underline + "\n\n ⎔".magenta + " Click above link to create a Github access token" + "\n\n ⎔".magenta + " Leave \"scope\" options as is, and click \"Generate token\" " + "\n\n ⎔".magenta + " This token will be stored locally and used whenever accessing a private repo" + "\n\n ⎔".magenta + " Paste token here:"
         }
-      ]
-      // get user to past github activity token with "repo" privileges
+      ];
+      // INFO: get user to past github activity token with "repo" privileges
       inquirer.prompt( questions, function( answers ) {
-        var tokenFinal = answers.token
+        var tokenFinal = answers.token;
         fs.outputJSON(configFile, {token: tokenFinal}, function(err){
           if (err) throw err
-          cb(null, tokenFinal, filterOutPR, currentRepoInfo)
-        })
-      })
+          cb(null, tokenFinal, filterOutPR, currentRepoInfo);
+        });
+      });
     } else if (!tokenCheck) {
-      // not private repo, no auth needed
-      tokenFinal = 'public'
-      cb(null, tokenFinal, filterOutPR, currentRepoInfo)
+      // INFO: not private repo, no auth needed
+      tokenFinal = 'public';
+      cb(null, tokenFinal, filterOutPR, currentRepoInfo);
     } else {
-      // token already exists, just read it
-      tokenFinal = tokenCheck
-      cb(null, tokenFinal, filterOutPR, currentRepoInfo)
+      // INFO: token already exists, just read it
+      tokenFinal = tokenCheck;
+      cb(null, tokenFinal, filterOutPR, currentRepoInfo);
     }
 
   },
 
   function githubAuthIfNeed(tokenFinal, filterOutPR, currentRepoInfo, cb) {
 
-    // skip github auth if public repo
+    // INFO: skip github auth if public repo
     if (tokenFinal === 'public') {
-      cb(null, filterOutPR)
+      cb(null, filterOutPR);
     } else if (tokenFinal) {
 
       github.authenticate({
           type: "oauth",
           token: tokenFinal
-      })
+      });
       github.issues.repoIssues({
           user: currentRepoInfo.username,
           repo: currentRepoInfo.repo,
@@ -202,133 +218,139 @@ async.waterfall([
           per_page: 100
       }, function(err, res) {
           if (err) {
-            // bad credentials
-            fs.remove(configFile, function(err){if (err) throw err})
+            // INFO: bad credentials
+            fs.remove(configFile, function(err){if (err) throw err});
           } else {
-            // good credentials
-            var filterOutPR = []
+            // INFO: good credentials
+            var filterOutPR = [];
 
             res.forEach(function (issue) {
               if (!issue.pull_request) {
-                filterOutPR.push(issue)
+                filterOutPR.push(issue);
               }
-            })
-            cb(null, filterOutPR)
+            });
+            cb(null, filterOutPR);
           }
-      })
+      });
     }
   },
 
-  // create a file for each issue in /issues
+  // INFO: create a file for each issue in /issues
   function createIssueFiles (filterOutPR, cb) {
 
-    var commentsURL = []
+    var commentsURL = [];
     filterOutPR.forEach(function (issue) {
-      // slugify title to get rid of characters that can cause filename problems
-      var issueFilename = util.format('issues/%s-%s.md', String(issue.number), slug(issue.title))
-      // template strings use indents, to avoid indents we must forgo code readability
+
+      // INFO: slugify title to get rid of characters that can cause filename problems
+      var issueFilename = util.format('issues/%s-%s.md', String(issue.number), slug(issue.title));
+
+      // TODO: fix this, it's bad
+      // INFO: template strings use indents, to avoid indents we must forgo code readability
       var finalIssue = `${issue.title}
 Issue filed by: ${issue.user.login}
 ${Date(issue.created_at)}
 
 ${issue.body}
 -------------------------------------------------------------------------------
-`
+`;
 
-      console.log('⭐️  #%s: %s', issue.number, issue.title.cyan)
+      console.log('⭐️  #%s: %s', issue.number, issue.title.cyan);
 
       fs.writeFile(issueFilename, finalIssue, function (error) {
         if (error) {
-          cb(error, null)
+          cb(error, null);
         }
-      })
-      // make sure only get issues that have comments
-      // or else request to comments_url will be empty
+      });
+
+      // INFO: make sure only get issues that have comments
+      // INFO: else request to comments_url will be empty
       if (issue.comments > 0) {
-        commentsURL.push(issue.comments_url)
+        commentsURL.push(issue.comments_url);
       }
-    })
-    cb(null, commentsURL)
+    });
+    cb(null, commentsURL);
   },
 
-  // create a new array of responses from each comment url
-  // if a issue has a comment value > 0 it will get requested
+  // INFO: create a new array of responses from each comment url
+  // INFO: if a issue has a comment value > 0 it will get requested
   function getIssueComments (commentsURL, cb) {
-    // take array of comment urls and get content
-    // callURL at the top of this file does all the
-    // request work for each URL
+
+    // INFO: take array of comment urls and get content
+    // INFO: callURL at the top of this file does all the
+    // INFO: request work for each URL
     async.map(commentsURL, callURL, function (error, results) {
       if (error) {
-        cb(error, null)
+        cb(error, null);
       }
-      cb(null, results)
-    })
+      cb(null, results);
+    });
   },
 
-  // create obj to match issue comments and existing file
+  // INFO: create obj to match issue comments and existing file
   function setupIssueComments (results, cb) {
-    // list of files in /issues
-    var paths = {}
+
+    // INFO: list of files in /issues
+    var paths = {};
     fs.readdir('issues/', function (error, files) {
       if (error) {
         throw error
       }
 
-      // for each url with comments this create a property
-      // on the paths object so that we're able to easy
-      // find which file comments should go
+      // INFO: for each url with comments this create a property
+      // INFO: on the paths object so that we're able to easy
+      // INFO: find which file comments should go
       files.forEach(function (file) {
-        paths[ file.split('-')[0] ] = 'issues/' + file
-      })
+        paths[ file.split('-')[0] ] = 'issues/' + file;
+      });
 
-      cb(null, paths, results)
-    })
+      cb(null, paths, results);
+    });
   },
 
-  // where we actually write the comments to their correct
-  // issue file in /issues directory
+  // INFO: where we actually write the comments to their correct
+  // INFO: issue file in /issues directory
   function writeIssueComments (paths, results, cb) {
     results.forEach(function (comment) {
       comment.forEach(function (individualComment) {
-        var writeToFile = paths[ individualComment.issue_url.split('/')[7] ]
+        var writeToFile = paths[ individualComment.issue_url.split('/')[7] ];
         var commentFinal = `${individualComment.user.login}
 ${Date(individualComment.created_at)}
 
 ${individualComment.body}
 
 -------------------------------------------------------------------------------
-`
+`;
 
-        // append all comments to file
+        // INFO: append all comments to file
         fs.appendFile(writeToFile, commentFinal, function (error) {
           if (error) {
-            cb(error, null)
+            cb(error, null);
           }
-        })
-      })
-    })
-    cb(null, 'done writing comments'.red)
+        });
+      });
+    });
+    cb(null, 'done writing comments'.red);
   }
 
 ],
   function (err, result) {
     if (err) {
-      console.error(err)
-      return
+      console.error(err);
+      return;
     }
-    // fancy output
+    // INFO: fancy output
     figlet.text('got issues!', {
       horizontalLayout: 'default',
       verticalLayout: 'default'
     }, function (err, data) {
       if (err) {
-        console.log('Something went wrong...')
-        console.dir(err)
-        return
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
       }
-      console.log(data)
-      console.log('\n    check your new issues/ directory'.green)
-      console.log('    (issues/ was added to .gitignore)\n')
-    })
+      console.log(data);
+      console.log('\n    check your new issues/ directory'.green);
+      console.log('    (issues/ was added to .gitignore)\n');
+    });
   }
-)
+);
