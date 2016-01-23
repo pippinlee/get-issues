b889907df06e50f1bfb543f538fc88b5283a66be
afrolionx <mike@mikecorp.ca>
2016-01-23 01:24:38 -0500

    create function to remove token from service

--- src/wip-auth-small.js
+++ src/wip-auth-small.js
@@ -2,6 +2,7 @@
 
 const config = require('./config');
 const inquirer = require('inquirer');
+const async = require('async');
 
 inquirer.prompt(config.questions.twofactor, function(answers) {
   config.github.authenticate({
@@ -10,23 +11,59 @@ inquirer.prompt(config.questions.twofactor, function(answers) {
     password: answers.password
   });
 
-  config.github.authorization.create({
-    scopes: [
-      "repo",
-      "public_repo",
-    ],
-    note: 'get-issues token',
+  // config.github.authorization.create({
+  //   scopes: [
+  //     "repo",
+  //     "public_repo",
+  //   ],
+  //   note: 'get-issues token',
+  //   headers: {
+  //     'User-Agent': 'get-issues',
+  //     'X-GitHub-OTP': answers.code
+  //   }
+  // }, function auth_cb(err, res) {
+  //   if (err) {
+  //     console.log('\nerr: ', err);
+  //     console.log('\nerr.message: ', JSON.parse(err.message));
+  //     console.log('\nerr.code: ', err.code);
+  //   } else {
+  //     console.log('res: ', res);
+  //   }
+  // });
+  config.github.authorization.getAll({
     headers: {
       'User-Agent': 'get-issues',
       'X-GitHub-OTP': answers.code
     }
-  }, function auth_cb(err, res) {
+  }, function(err, res) {
     if (err) {
-      console.log('\nerr: ', err);
-      console.log('\nerr.message: ', JSON.parse(err.message));
-      console.log('\nerr.code: ', err.code);
+      console.log('err:', err);
     } else {
-      console.log('res: ', res);
+      // console.log('res:', res);
+      async.filter(
+        res,
+        function(item, callback) {
+          if (item.app.name === 'get-issues token') {
+            callback(true);
+          } else {
+            callback(false);
+          }
+        },
+        function(result) {
+          console.log('results from async:', result);
+          config.github.authorization.delete({
+            id: result[0].id,
+            headers: {
+              'User-Agent': 'get-issues',
+              'X-GitHub-OTP': answers.code
+            }
+          }, function(err, res) {
+            console.log('delete err:', err);
+            console.log('delete res:', res);
+          });
+        }
+      );
+      // console.log('app:', item.app.name === 'get-issues token');
     }
   });
 });
